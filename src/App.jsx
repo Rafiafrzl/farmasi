16e3dab12b8c45d99daa2a4c6cc93d57382955f4
Vinix <nzxtvinix@gmail.com>
2025-08-26 15:03:03 +0700

    Add Footer component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
