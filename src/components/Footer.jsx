d984ace03ff938886bf02305d5cb069435034168
Vinix <nzxtvinix@gmail.com>
2025-08-26 15:44:04 +0700

    wes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#0F9E98] text-white text-sm px-6 py-2 flex justify-between items-center fixed bottom-0 left-0 w-full z-50">
+    <footer className="bg-[#0F9E98] text-white text-sm px-6 py-2 flex justify-between items-center absolute -bottom-0 left-0 w-full z-50">
       <div className="flex items-center space-x-2">
         <img src="/logo-4-sm.png" alt="Logo" className="h-6" />
         <span>PT. INTI DAYA ENERGITAMA - MERSI HOSPITAL © 2020</span>
